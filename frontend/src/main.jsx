a9e0681936beecc64c736b261cd1b3855c749a53
neer-aj1 <neerajkumar15439@gmail.com>
2024-07-01 21:32:44 +0530

    configure redux store for login and logout

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
     <ToastContainer />
   </React.StrictMode>
 );
