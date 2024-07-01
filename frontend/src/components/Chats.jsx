a7351a8f2c63c755cdcd6761c8cf3b0dae088492
neer-aj1 <neerajkumar15439@gmail.com>
2024-07-01 23:23:31 +0530

    add selected chat with redux

--- frontend/src/components/Chats.jsx
+++ frontend/src/components/Chats.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectChat } from "../redux/slices/chatSelectSlice";
 
-const Chats = ({chat}) => {
+const Chats = ({ chat }) => {
+  const dispatch = useDispatch();
+  const selectedChatId = useSelector((state) => state.selectChat.selectedChat._id);
+
+  const handleClick = () => {
+    dispatch(selectChat(chat));
+  };
+
+  const isSelected = selectedChatId === chat._id;
+
   return (
-    <div className='border min-w-72 border-white rounded-xl flex items-center gap-3 p-2'>
-        <img className='w-14 rounded-full h-14' src={chat.profilePic} />
-        <p className='flex-1'>{chat.name}</p>
+    <div
+      onClick={handleClick}
+      className={`border min-w-72 ${isSelected ? "bg-blue-500 text-white" : ""} border-white rounded-xl flex items-center gap-3 p-2 cursor-pointer hover:bg-blue-500 hover:text-white`}
+    >
+      <img className="w-14 rounded-full h-14" src={chat.profilePic} alt="Profile" />
+      <p className="flex-1">{chat.name}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Chats
+export default Chats;
