5a5c7f2e580a1ee307667e0d1ae828c0cf971afe
neer-aj1 <neerajkumar15439@gmail.com>
2024-07-02 08:57:05 +0530

    align messages according to sender

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
-const Message = ({message}) => {
+const Message = ({ message }) => {
+  const senderId = message.receiverId;
+  const loggedInUserId = useSelector((state) => state.user?.user?._id);
+  const right = senderId === loggedInUserId;
   return (
-    <div className='bg-blue-500 p-1 w-fit rounded-t-xl rounded-br-xl'>
-      <p>{message.message}</p>
+    <div className={`bg-red-500 w-full flex ${right ? 'justify-end': ''}`}>
+      <div className={`bg-blue-500 p-1 w-fit rounded-t-xl ${right? 'rounded-bl-xl':'rounded-br-xl'}`}>
+        <p>{message.message}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
