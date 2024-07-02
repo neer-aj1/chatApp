70d66651d499443baee146f924a8130aa0776f08
neer-aj1 <neerajkumar15439@gmail.com>
2024-07-02 08:13:21 +0530

    add home screen gap

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import MessageContainer from "../components/MessageContainer";
 const Home = () => {
   return (
     <div className="h-screen w-screen bg-yellow-400 flex flex-row items-center justify-center">
-      <div className="bg-white h-[80%] w-[50%] bg-opacity-50 px-5 py-5 shadow-3xl backdrop-blur-md flex gap-6 p-10 rounded-2xl">
+      <div className="bg-white h-[80%] w-[50%] bg-opacity-50 px-5 py-5 shadow-3xl backdrop-blur-md flex gap-1 p-10 rounded-2xl">
         <div className="min-w-[30%]">
           <Sidebar />
         </div>
